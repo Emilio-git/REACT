b5d698b573cb18e8b8e3c174a196ef76df683efa
Emil <e.yakoff4@gmail.com>
2022-10-12 21:57:24 +0300

    v0.2

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -6,7 +6,7 @@ import Employerslist from '../employers-list/employers-list';
 import EmployersAddForm from '../employers-add-form/employers-add-form';
 
 const data = [
-   {name: 'Эмиль', salary: 3000, increase: true, id: 1},
+   {name: 'Эмиль', salary: 100, increase: true, id: 1},
    {name: 'Антон', salary: 5000, increase: true, id: 2},
    {name: 'Карл', salary: 1100, increase: false, id: 3},
 ]
